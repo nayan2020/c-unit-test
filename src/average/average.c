42aa7fbd9ebe1e245f03786503932e55842f0a52
demo <rajnikag6@gmail.com>
2024-06-04 23:03:01 +0530

    update

--- src/average/average.c
+++ src/average/average.c
@@ -1,6 +1,10 @@
 #include "average.h"
 
 float average(float arr[], unsigned int size){
+    if(size == 0){
+        return 0.0;
+    }
+
     float total = 0;
 
     for(unsigned int n = 0; n < size; n++){
